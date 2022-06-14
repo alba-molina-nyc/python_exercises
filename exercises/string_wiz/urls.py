ec66774f4e07762a4ef285239c0ab27dda54b8ee
Username <albavmolinanyc@gmail.com>
2022-06-14 16:13:19 -0400

    add def view

--- exercises/string_wiz/urls.py
+++ exercises/string_wiz/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # path('url_route_name', views_page.url_name, name='url_name'),
     path('', views.home, name='home'),
     path('<int:Translation_Question_id>/', views.detail, name='detail'),
+    path('<int:Translation_Question_id>/pig_latinfied/', views.pig_latinfied, name='pig_latinfied'),
    
     
 ]
--- exercises/string_wiz/views.py
+++ exercises/string_wiz/views.py
@@ -7,5 +7,9 @@ def home(request):
     return render(request, 'base.html', {})
 
 def detail(request, Translation_Question_id):
-    response = "you are looking at the results for question %s."
-    return HttpResponse(Translation_Question_id)
+    response = "You are 👀 at where the user is going to enter their ❓ #️⃣ %s before it is translated from 💂🏻‍♀️ English 💂🏻‍♀️ ➡️ 🐽Pig Latin🐽"
+    return HttpResponse(response % Translation_Question_id)
+
+def pig_latinfied(request, Translation_Question_id):
+    response = "You are 👀 at where the translation is going to go for ❓ #️⃣ %s, from 💂🏻‍♀️ English 💂🏻‍♀️ ➡️ 🐽Pig Latin🐽"
+    return HttpResponse(response % Translation_Question_id)
