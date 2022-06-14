bbccf47478cbd3263201fd62fee632ff65ba9b47
Username <albavmolinanyc@gmail.com>
2022-06-14 15:45:31 -0400

    adds a notes folder

--- exercises/string_wiz/models.py
+++ exercises/string_wiz/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import datetime
+from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,6 +12,12 @@ class Translation_Question(models.Model):
     def __str__(self):
         return self.translation_question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Translation_Answer(models.Model):
     translation_question_text = models.ForeignKey(Translation_Question, on_delete=models.CASCADE, default=0)
     translation_answer_text = models.CharField(max_length=10000)
+
+    def __str__(self):
+        return self.translation_answer_text
