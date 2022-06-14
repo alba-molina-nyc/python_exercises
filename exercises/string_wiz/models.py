29f83928eb38b243279e8700c54c83fcff11c709
Username <albavmolinanyc@gmail.com>
2022-06-14 14:54:01 -0400

    add notes on psql

--- exercises/string_wiz/models.py
+++ exercises/string_wiz/models.py
@@ -6,6 +6,9 @@ class Translation_Question(models.Model):
     translation_question_text = models.CharField(max_length=4000)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.translation_question_text
+
 class Translation_Answer(models.Model):
     translation_question_text = models.ForeignKey(Translation_Question, on_delete=models.CASCADE, default=0)
     translation_answer_text = models.CharField(max_length=10000)
